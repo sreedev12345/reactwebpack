6a0ed370156d93ca7e55f457dd4a34507ed7ef22
Sree Dev <sreedev.developer@gmail.com>
2021-07-04 10:50:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<h1>react application</h1>,document.getElementById("root"));
+ReactDOM.render(<h1>react applicationssss</h1>,document.getElementById("root"));
